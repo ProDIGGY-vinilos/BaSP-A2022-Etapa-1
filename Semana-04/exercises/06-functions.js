e674a71fdeb3e19b4d97cb5fcf8a9b8ab34f2df0
ProDIGGY-vinilos <cristiangerster@gmail.com>
2022-09-20 15:50:43 -0300

    BaSP-W04-2022: Mobile first done

--- Semana-04/exercises/06-functions.js
+++ Semana-04/exercises/06-functions.js
@@ -6,20 +6,20 @@ console.log('-Exercise 6.a:');
 function suma(num1, num2) {
     return (num1 + num2);
 }
-var resultFunction = suma(5, 6);
+var resultFunction = suma(5, 6.55);
 console.log(resultFunction);
 // A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número;
 // de no ser un número, mostrar una alerta aclarando que uno de los parámetros tiene error y
 // retornar el valor NaN como resultado.
 console.log('-Exercise 6.b:');
-function suma(num1, num2) {
+function suma1(num1, num2) {
     if (typeof num1 != 'number' || typeof num2 != 'number'){
         alert('One or two parameters are not typeof number');
         return 'NaN';
     } else
     return (num1 + num2);
 }
-resultFunction = suma(5, 'l');
+resultFunction = suma1(5, '1');
 console.log(resultFunction);
 // Aparte, crear una función validate Integer que reciba un número como parámetro y
 // devuelva verdadero si es un número entero.
@@ -31,31 +31,39 @@ function validate(numbr) {
 // que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y
 // retornar el número convertido a entero (redondeado).
 console.log('-Exercise 6.d:');
-function suma(num1, num2) {
+function suma2(num1, num2) {
     if (typeof num1 != 'number' || typeof num2 != 'number'){
         alert('One or two parameters are not typeof number');
         return 'NaN';
     } else{
-        if (validate(num1) && validate(num2)) {
+        if (validate(num1)) {
         } else{
+            alert('Number: '+ num1+' is decimal, rounding it...');
             num1 = Math.round(num1);
+            return num1;
+        }
+        if (validate(num2)) {
+        } else{
+            alert('Number: '+ num2+' is decimal, rounding it...');
             num2 = Math.round(num2);
+            return num2;
         }
         return (num1 + num2);
     }
 }
-console.log(suma(5, 5.55));
+console.log(suma2(6, 5.55));
 // Convertir la validación del ejercicio 6d) en una función separada y
 // llamarla dentro de la función suma probando que todo siga funcionando igual.
 console.log('-Exercise 6.e:');
 function validationDecimals(num1) {
     if (validate(num1)) {
     } else{
+        alert('Number: '+ num1+' is decimal, rounding it...');
         num1 = Math.round(num1);
     }
     return num1;
 }
-function suma(num1, num2) {
+function suma3(num1, num2) {
     if (typeof num1 != 'number' || typeof num2 != 'number'){
         alert('One or two parameters are not typeof number');
         return 'NaN';
@@ -65,4 +73,4 @@ function suma(num1, num2) {
         return (num1 + num2);
     }
 }
-console.log(suma(6, 6.55));
+console.log(suma3(6, 6.55));
