660ed9d05ffbd4f0308ec991c86717bbc152f880
ProDIGGY-vinilos <cristiangerster@gmail.com>
2022-09-26 02:42:37 -0300

    BaSP-W06: Validation Signup done

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
     var motiveError, inputContent, inputSelector, validateinputContent;
+    var validateEmailContent;
     // general methods
     function correctLength(inputC ,cant) {
         if (inputC.length <= cant) {
@@ -13,12 +14,12 @@ window.onload = function(){
             input.classList.add('ok-input');
             motiveError = '';
         } else  {   input.classList.add('error-input'); }
-    }
-    function removeStyles(input) {
         if (input.value == '') {
             input.classList.remove('ok-input');
             input.classList.remove('error-input');
         }
+    }
+    function removeStyles(input) {
         input.classList.remove('ok-input');
         input.classList.remove('error-input');
     }
@@ -51,7 +52,7 @@ window.onload = function(){
         if (inputC.length < firstNum || inputC.length > secondNum) {
             validateinputContent = false;
             motiveError ='The content field doesnt reach the range of characters';
-        }
+        } else {validateinputContent = true; }
     }
     function hasNumAndLetters(inputC) {
         for(var i = 0; i < inputC.length; i++){
@@ -72,16 +73,16 @@ window.onload = function(){
     function hasNumOrLetters(inputC) {
         var hasLetter = false;
         var hasNumber = false;
-        inputC = inputC.split(' ');
-        for (var j = 0; j < inputC.length; j++) {
-            for(var i = 0; i < inputC[i].length; i++){
-                if((inputC[i].charCodeAt(i) >= 97) && (inputC[i].charCodeAt(i) <= 122)){// lowercase letters
+        var inputArray = inputC.split(' ');
+        for (var input of inputArray) {
+            for (var i = 0; i < input.length; i++) {
+                if((input.charCodeAt(i) >= 97) && (input.charCodeAt(i) <= 122)){// lowercase letters
                     validateinputContent = true;
                     hasLetter = true;
-                } else if((inputC[i].charCodeAt(i) >= 65) && ((inputC[i].charCodeAt(i) <= 90))){ //uppercase letters
+                } else if((input.charCodeAt(i) >= 65) && ((input.charCodeAt(i) <= 90))){ //uppercase letters
                     validateinputContent = true;
                     hasLetter = true;
-                } else if((inputC[i].charCodeAt(i) >= 48) && (inputC[i].charCodeAt(i) <= 57)){// numbers
+                } else if((input.charCodeAt(i) >= 48) && (input.charCodeAt(i) <= 57)){// numbers
                     validateinputContent = true;
                     hasNumber = true;
                 }
@@ -90,17 +91,16 @@ window.onload = function(){
                     motiveError = 'The content field has invalid characters';
                     break;
                 }
-            }
-            if (hasLetter && hasNumber) {
-                validateinputContent = false;
-                motiveError = 'The content field has invalid characters';
-                break;
-            }
+                }
+                if (hasLetter && hasNumber) {
+                    validateinputContent = false;
+                    motiveError = 'The content field has invalid characters';
+                    break;
+                }
+            hasLetter = false;
+            hasNumber = false;
         }
     }
-
-    // AGREGAR VALIDATESTYLE A ONBLUR FALTANTES
-
     //validate NAME
     var inputName = document.querySelector('[name="name"]');
     inputName.onblur = function(){
@@ -138,6 +138,25 @@ window.onload = function(){
         removeStyles(inputDni);
     }
     //validate Birthdate
+    var inputBirthdate = document.querySelector('[name="birthdate"]');
+    inputBirthdate.onblur = function() {
+        removeStyles(inputBirthdate);
+        var birthdateContent = new Date((inputBirthdate.value));
+        var monthDiff = Date.now() - birthdateContent.getTime();
+        var ageDateTime = new Date(monthDiff);
+        var year = ageDateTime.getUTCFullYear();
+        var age = Math.abs(year - 1970);
+        if (age > 18 && age < 100) {
+            validateinputContent = true;
+        } else {
+            validateinputContent = false;
+            motiveError = 'Invalid age';
+        }
+        styleValidation(inputBirthdate, validateinputContent);
+    }
+    inputBirthdate.onfocus = function(){
+        removeStyles(inputBirthdate);
+    }
     //validate Phone
     var inputPhone = document.querySelector('[name="phone"]');
     inputPhone.onblur = function(){
@@ -153,12 +172,13 @@ window.onload = function(){
     //validate Address
     var inputAddress = document.querySelector('[name="address"]');
     inputAddress.onblur = function(){
-        var addressContent = (inputAddress.value).split(' ').join('');
+        var addressContent = ((inputAddress.value).split(' ')).join('');
         removeStyles(inputAddress);
         correctLength(addressContent, 5);
         hasNumAndLetters(addressContent);
-        addressContent = inputAddress.value;
+        addressContent = (inputAddress.value);
         hasNumOrLetters(addressContent);
+        styleValidation(inputAddress, validateinputContent);
     }
     inputAddress.onfocus = function(){
         removeStyles(inputAddress);
@@ -170,6 +190,8 @@ window.onload = function(){
         removeStyles(inputLocation);
         correctLength(locationContent, 3);
         hasNumAndLetters(locationContent);
+        styleValidation(inputLocation, validateinputContent);
+
     }
     inputLocation.onfocus = function(){
         removeStyles(inputLocation);
@@ -183,12 +205,51 @@ window.onload = function(){
         rangeLength(pcContent, 4, 5);
         styleValidation(inputPC, validateinputContent);
     }
+    inputPC.onfocus = function(){
+        removeStyles(inputPC);
+    }
     //validate EMAIL
     var inputEmail = document.querySelector('[name="email"]');
     inputEmail.onblur = function() {
-        if((emailInput.value).match(emailExpression)){
-            
+        removeStyles(inputEmail);
+        if ((inputEmail.value).match(emailExpression)) {
+            validateEmailContent = true;
+        } else {
+            validateEmailContent = false;
         }
+        styleValidation(inputEmail, validateEmailContent);
+    }
+    inputEmail.onfocus = function(){
+        removeStyles(inputEmail);
+    }
+    //validate PASSWORD
+    var inputPass = document.querySelector('[name="pass"]');
+    inputPass.onblur = function() {
+        var passContent = inputPass.value;
+        removeStyles(inputPass);
+        correctLength(passContent, 7);
+        hasNumAndLetters(passContent);
+        styleValidation(inputPass, validateinputContent);
+    }
+    inputPass.onfocus = function(){
+        removeStyles(inputPass);
+    }
+    //validate PASSWORD REPEAT
+    var inputPassR = document.querySelector('[name="passr"]');
+    inputPassR.onblur = function() {
+        var passRContent = inputPassR.value;
+        var passContent = (document.querySelector('[name="pass"]')).value;
+        removeStyles(inputPassR);
+        if ((passRContent).match(passContent)) {
+            validateinputContent = true;
+        } else {
+            validateinputContent = false;
+            motiveError = 'Password doesnt match';
+        }
+        styleValidation(inputPassR, validateinputContent);
+    }
+    inputPassR.onfocus = function(){
+        removeStyles(inputPassR);
     }
     //reset fields
     var resetBtn = document.querySelector('#reset-btn');
